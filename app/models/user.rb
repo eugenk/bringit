287ed735c6e10511b7bbf6bd81fed331d1eb3d4d
jalyna <jalyna@informatik.uni-bremen.de>
2013-02-20 15:43:17 +0100

    Finish user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  before_save { email.downcase! }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8, maximum: 128 }
+  validates :password_confirmation, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
   before do
     @user = User.new(email: "eugenk@tzi.de", 
-                     password: "foobar", password_confirmation: "foobar")
+                     password: "password", password_confirmation: "password")
   end
   
   subject { @user }
@@ -63,22 +63,6 @@ describe User do
   describe "when password confirmation is nil" do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
-  end
-  
-  describe "return value of authenticate method" do
-    before { @user.save }
-    let(:found_user) { User.find_by_email(@user.email) }
-  
-    describe "with valid password" do
-      it { should == found_user.authenticate(@user.password) }
-    end
-  
-    describe "with invalid password" do
-      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-  
-      it { should_not == user_for_invalid_password }
-      specify { user_for_invalid_password.should be_false }
-    end
   end
   
   describe "with a password that's too short" do
