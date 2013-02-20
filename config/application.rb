18738e2b5d0e056e43486207572a0e0a9b540912
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-02-20 13:39:06 +0100

    Configure the Rails Generator

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ end
 
 module Awe11
   class Application < Rails::Application
+    
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
