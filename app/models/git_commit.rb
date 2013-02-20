6258550b8d9f34428a5b4b52b73d909816290c8f
jalyna <jalyna@informatik.uni-bremen.de>
2013-02-20 20:44:53 +0100

    Setup GitCommit Model

--- app/models/git_commit.rb
+++ app/models/git_commit.rb
@@ -1,3 +1,11 @@
 class GitCommit < ActiveRecord::Base
-  attr_accessible :author_email, :author_name, :author_time, :committer_email, :committer_name, :committer_time, :git_push_id, :commit_hash, :message
+  attr_accessible :author_email, :author_name, :author_time, :committer_email, :committer_name, 
+    :committer_time, :git_push_id, :commit_hash, :message
+  
+  belongs_to :git_push
+  has_one :git_repository, through: :git_push
+  
+  VALID_HASH_REGEX = /^[0-9a-f]+$/i
+  validates :commit_hash, presence: true, length: { maximum: 40, minimum: 40 }, format: VALID_HASH_REGEX
+  validates :message, presence: true
 end
