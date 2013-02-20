f918e2f47a70b2772babb7e03e2cbf1d848bb3c7
jalyna <jalyna@informatik.uni-bremen.de>
2013-02-20 13:44:17 +0100

    Set layout links

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,22 +1,29 @@
 require 'spec_helper'
 
-describe "Home" do
+describe "Pages" do
   subject { page }
   
-  shared_examples_for "all home pages" do
+  shared_examples_for "all pages" do
     it { should have_selector('h1',    text: heading) }
     it { should have_selector('title', text: full_title(page_title)) }
   end
   
-  describe " page" do
+  describe "Home" do
     before { visit root_path }
     
     let(:heading)    { 'Home' }
     let(:page_title) { '' }
     
-    it_should_behave_like 'all home pages'
+    it_should_behave_like 'all pages'
   end
   
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "Sign up"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "Sign in"
+    page.should have_selector 'title', text: full_title('Sign in')
+  end
   # for later use
   #it "should have the right links on the layout" do
   #  visit root_path
