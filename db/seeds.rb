0a22f6327e438617c42672b441c09bc7f60e3a38
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-02-21 13:24:35 +0100

    Repo seeds - model broken at this point

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,39 @@
     password_confirmation: "password#{n}"
   })
 end
+
+
+5.times do
+  repo = GitRepository.create({
+    title: Faker::Lorem.words(rand(2)+1),
+    description: Faker::Lorem.paragraph(rand(3)+1),
+    owners: [User.first(offset: rand(User.count))],
+    path: Faker::Lorem.words(rand(4)+1).split(' ').join('/')
+  })
+  
+  
+  5.times do
+    push = GitPush.create({
+      author: repo.owners.first,
+      push_type: 'web',
+      git_repository: repo
+    })
+    
+    3.times do
+      create_commit([create_commit, create_commit])
+    end
+  end
+end
+
+
+def create_commit(parents = [])
+  GitCommit.create({
+    commit_hash: Faker::Base.regexify(/^[a-f0-9]{40}$/),
+    committer_name: Faker::Name.name,
+    committer_email: Faker::Internet.email,
+    committer_time: Time.at(rand_in_range(28.days.ago, Time.now)),
+    message: Faker::Lorem.sentences(rand(2)+1), #TODO: long commit messages
+    git_push: push,
+    parents: parents
+  })
+end
