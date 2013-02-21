a80c2d493b0564932a16b7a8c8468b1e8b49b878
jalyna <jalyna@informatik.uni-bremen.de>
2013-02-21 09:21:54 +0100

    Add validation

--- app/models/git_repository.rb
+++ app/models/git_repository.rb
@@ -1,3 +1,7 @@
 class GitRepository < ActiveRecord::Base
   attr_accessible :description, :path, :title
+  
+  validates :title, presence: true
+  VALID_PATH_REGEX = /^((?:[a-zA-Z0-9]+(?:_[a-zA-Z0-9]+)*(?:\-[a-zA-Z0-9]+)*)+)$/
+  validates :path, presence: true, uniqueness: { case_sensitive: true }, format: VALID_PATH_REGEX
 end
