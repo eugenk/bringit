f636a2cab5da005b8f35c5e1a6ac92b840aa9584
jalyna <jalyna@informatik.uni-bremen.de>
2013-02-21 13:54:25 +0100

    Remove validation from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,4 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  before_save { email.downcase! }
-  
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 8, maximum: 128 }
-  validates :password_confirmation, presence: true
 end
