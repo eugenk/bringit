4aa23f85065db5a9a75af3fbf1a9cd87c26f1152
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-02-21 19:01:52 +0100

    Limit commits assotiation

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,10 +1,10 @@
 class Repository < ActiveRecord::Base
-  attr_accessible :description, :path, :title, :owners, :pushes
+  attr_accessible :description, :path, :title, :owners, :pushes, :commits
   
   has_many :repository_owners, foreign_key: 'repository_id', class_name: 'RepositoryOwner'
   has_many :owners, through: :repository_owners, class_name: 'User'
   has_many :pushes, class_name: 'Push', foreign_key: 'repository_id'
-  has_many :commits, class_name: 'Commit', through: :pushes
+  has_many :commits, class_name: 'Commit', through: :pushes, :limit => 3
   
   validate :validate_owner_existance
   validates :title, presence: true
