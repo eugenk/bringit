9a661bb24af3f89aa2e34680a66f51608240e722
jalyna <jalyna@informatik.uni-bremen.de>
2013-02-21 13:05:15 +0100

    Add Git Repository to push

--- app/models/git_push.rb
+++ app/models/git_push.rb
@@ -1,7 +1,7 @@
 class GitPush < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
-  
-  attr_accessible :push_type, :author
+  belongs_to :git_repository
+  attr_accessible :push_type, :author, :git_repository
   
   validates :push_type, presence: true, included_in: ['web', 'ssh']
 end
