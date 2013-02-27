ba60f913ab767e69f01f9a4ab29bc8e9fbfa1c65
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-02-27 15:58:45 +0100

    restore deploy config: git repositories into /tmp/gitroot

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,8 +49,10 @@ namespace :deploy do
     run "ln -fs #{shared_path}/secret_token.rb #{latest_release}/config/initializers/"
   end
   task :reset do
-    run "rm -rf #{File.join(current_path,'data','gitroot')}"
-    run "mkdir -p #{File.join(current_path,'data','gitroot')}"
+    run "rm -rf /tmp/gitroot"
+    run "mkdir -p /tmp/gitroot"
+    #run "rm -rf #{File.join(current_path,'data','gitroot')}"
+    #run "mkdir -p #{File.join(current_path,'data','gitroot')}"
     run "cd #{current_path} && bundle exec rake db:migrate:reset db:seed", env: { RAILS_ENV: 'production' }
   end
   task :init do
