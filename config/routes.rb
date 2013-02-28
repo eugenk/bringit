3266bfb6fe73594421f9b92f89f3ae9028364740
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-02-28 14:23:49 +0100

    change match routes to get routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,23 +12,23 @@ Bringit::Application.routes.draw do
     post 'upload' => "repositories#upload"
   end
   
-  match '/repositories/:id/:url' => "repositories#show", constraints: { url: /.*/ }, as: :browse_repository
+  get '/repositories/:id/:url' => "repositories#show", constraints: { url: /.*/ }, as: :browse_repository
   
-  match '/raw/repositories/:id/:url' => "repositories#raw", constraints: { url: /.*/ }, as: :raw_repository
-  match '/raw/commits/:id/:oid/:url' => "repositories#raw", constraints: { url: /.*/ }, as: :raw_commits
+  get '/raw/repositories/:id/:url' => "repositories#raw", constraints: { url: /.*/ }, as: :raw_repository
+  get '/raw/commits/:id/:oid/:url' => "repositories#raw", constraints: { url: /.*/ }, as: :raw_commits
   
   post '/repositories/:id' => "repositories#update"
   delete '/delete_file/repositories/:id/:url' => "repositories#delete_file", constraints: { url: /.*/ }, as: :delete_file_repository
   post '/update_file/repositories/:id/:url' => "repositories#update_file", constraints: { url: /.*/ }, as: :update_file_repository
   
-  match '/commits/:id/:oid' => "repositories#show", as: :browse_commits_root
-  match '/commits/:id/:oid/:url' => "repositories#show", constraints: { url: /.*/ }, as: :browse_commits
+  get '/commits/:id/:oid' => "repositories#show", as: :browse_commits_root
+  get '/commits/:id/:oid/:url' => "repositories#show", constraints: { url: /.*/ }, as: :browse_commits
   
-  match '/commits/:id' => "repositories#commits", as: :commits
-  match '/entries/:id' => "repositories#entries_info", as: :entries
+  get '/commits/:id' => "repositories#commits", as: :commits
+  get '/entries/:id' => "repositories#entries_info", as: :entries
   
-  match '/history/repositories/:id/:url' => "repositories#history", constraints: { url: /.*/ }, as: :history_repository
-  match '/history/commits/:id/:oid/:url' => "repositories#history", constraints: { url: /.*/ }, as: :history_commits
+  get '/history/repositories/:id/:url' => "repositories#history", constraints: { url: /.*/ }, as: :history_repository
+  get '/history/commits/:id/:oid/:url' => "repositories#history", constraints: { url: /.*/ }, as: :history_commits
   
-  match '/search', to: 'repositories#search'
+  get '/search', to: 'repositories#search'
 end
