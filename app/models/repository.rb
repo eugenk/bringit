ab3bf9782672311712ae06429b9b11ac4d8b7ab1
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-03-02 17:28:34 +0100

    modularize #3: entry info

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,7 +1,7 @@
 require 'digest/sha1'
 
 class Repository < ActiveRecord::Base
-  include CommitGettable, ObjectGettable, FolderContentsGettable, Committable
+  include CommitGettable, ObjectGettable, FolderContentsGettable
   attr_accessible :description, :title, :owners
   attr_protected :path, :pushes, :commits, :repo
   
@@ -68,6 +68,56 @@ class Repository < ActiveRecord::Base
     raise RepositoryNotFoundError
   end
   
+  def delete_file(user, target_path)
+    commit_file(user, nil, target_path, "Delete file #{target_path}")
+  end
+
+  def add_file(user, tmp_path, target_path, message)
+    commit_file(user, File.open(tmp_path, 'rb').read, target_path, message)
+  end
+  
+  def commit_file(user, file_contents, target_path, message)
+    #Entry
+    entry = nil
+    if file_contents
+      entry = {
+        type: :blob, 
+        name: nil, 
+        oid: Rugged::Blob.create(repo, file_contents), 
+        content: file_contents,
+        filemode: 33188
+      }
+    end
+    
+    # TreeBuilder
+    if repo.empty?
+      old_tree = nil
+    else
+      old_tree = head.tree 
+    end
+    
+    tree = build_tree(entry, old_tree, target_path.split('/'))
+    
+    # Commit Sha
+    commit_oid = Rugged::Commit.create(repo, author: user.author, 
+      message: message, committer: user.author, parents: commit_parents, tree: tree)
+    rugged_commit = repo.lookup(commit_oid)
+    
+    if repo.empty?
+      ref = Rugged::Reference.create(repo, 'refs/heads/master', commit_oid)
+    else
+      repo.head.target = commit_oid
+    end
+    
+    touch
+    
+    push = build_push(user)
+    commit = build_commit(user, push, rugged_commit)
+    commit.save
+    
+    commit
+  end
+  
   def path_exists?(commit_oid=nil, url='')
     rugged_commit = get_commit(commit_oid)
     if !rugged_commit && url.empty?
@@ -120,33 +170,6 @@ class Repository < ActiveRecord::Base
     commit_oid == nil || (!repo.empty? && commit_oid == head_oid)
   end
 
-  def entries_info(commit_oid=nil, url_path='')
-    rugged_commit = get_commit(commit_oid)
-    if !rugged_commit && url_path.empty?
-      []
-    else
-      contents = folder_contents_rugged(rugged_commit, url_path)
-      contents.map do |e|
-        file_path = build_target_path(url_path, e[:name])
-        entry_info_rugged(rugged_commit, file_path)
-      end
-    end
-  end
-
-  def entry_info(url, commit_oid=nil)
-    rugged_commit = get_commit(commit_oid)
-    entry_info_rugged(get_commit(commit_oid), url)
-  end
-  
-  def entry_info_list(url, commit_oid=nil)
-    rugged_commit = get_commit(commit_oid)
-    if !rugged_commit
-      []
-    else
-      entry_info_list_rugged(rugged_commit, url)
-    end
-  end
-
   def get_changed_files(commit_oid=nil)
     rugged_commit = get_commit(commit_oid)
     if !rugged_commit
@@ -159,47 +182,6 @@ class Repository < ActiveRecord::Base
   # PROTECTED METHODS
 
   protected
-
-  def entry_info_rugged(rugged_commit, url)
-    object = get_object(rugged_commit, url)
-    changing_rugged_commit = get_commit_of_last_change(url, object.oid, rugged_commit)
-    build_entry_info(changing_rugged_commit, url)
-  end
-  
-  def build_entry_info(changing_rugged_commit, url)
-    {
-      committer_name: changing_rugged_commit.committer[:name],
-      committer_email: changing_rugged_commit.committer[:email],
-      committer_time: changing_rugged_commit.committer[:time].iso8601,
-      message: Commit.message_title(changing_rugged_commit.message),
-      oid: changing_rugged_commit.oid,
-      filename: url.split('/')[-1]
-    }
-  end
-  
-  def entry_info_list_rugged(rugged_commit, url)
-    entries = []
-    
-    changing_rugged_commit = get_commit_of_last_change(url, nil, rugged_commit)
-    previous_rugged_commit = nil
-    
-    until changing_rugged_commit == previous_rugged_commit || !changing_rugged_commit do
-      entries << changing_rugged_commit.oid
-      
-      previous_rugged_commit = changing_rugged_commit
-      
-      unless changing_rugged_commit.parents.empty?
-        changing_rugged_commit = get_commit_of_last_change(url, nil, changing_rugged_commit.parents.first)
-      end
-    end
-    
-    # file does not exist in inital commit
-    if changing_rugged_commit && !path_exists_rugged?(changing_rugged_commit, url)
-      entries[0..-2]
-    else
-      entries
-    end
-  end
 
   def get_changed_files_rugged(rugged_commit)
     if rugged_commit.parents.empty?
@@ -324,28 +306,6 @@ class Repository < ActiveRecord::Base
 
   def mime_type_editable?(mime_type)
     mime_type.to_s == 'application/xml' || mime_type.to_s.match(/^text\/.*/)
-  end
-  
-  def get_commit_of_last_change(url, previous_entry_oid=nil, rugged_commit=nil, previous_rugged_commit=nil)
-    rugged_commit ||= head
-
-    object = get_object(rugged_commit, url)
-    object_oid = object ? object.oid : nil
-    
-    previous_entry_oid ||= object_oid unless previous_rugged_commit
-    
-    if object_oid == previous_entry_oid
-      if rugged_commit.parents.empty?
-        rugged_commit
-      else
-        parents = rugged_commit.parents.sort_by do |p|
-          get_commit_of_last_change(url, previous_entry_oid, p, rugged_commit).committer[:time]
-        end
-        get_commit_of_last_change(url, previous_entry_oid, parents[-1], rugged_commit)
-      end
-    else
-      previous_rugged_commit
-    end
   end
 
   def get_commit(commit_oid=nil)
